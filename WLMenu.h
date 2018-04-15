286c47c22f5e1a94a0977e910451e67ec0f7cae9
dachuiwang <dachuiwang@dachuiwangdeiMac.local>
2018-04-15 11:56:56 +0800

    commit

--- WLMenu.h
+++ WLMenu.h
@@ -8,13 +8,81 @@
 
 #import <UIKit/UIKit.h>
 #define Gray [UIColor colorWithRed:154/256.0 green:154/256.0 blue:154/256.0 alpha:1] //
+#define Red [UIColor colorWithRed:171/256.0 green:36/256.0 blue:35/256.0 alpha:1] //
+
+
+@protocol WLMenuDelegate <NSObject>
+
+
+// 反馈
+- (void)WLMenu_advise;// 咨询建议
+- (void)WLMenu_bookerrorWithIndex:(NSInteger)index;// 书籍报错
+
+// 缓存
+- (void)WLMenu_download50;// 缓存50
+- (void)WLMenu_download100;// 缓存100
+
+// 设置
+- (void)WLMenu_setting_lightWithValue:(CGFloat)value;// 亮度
+- (void)WLMenu_setting_fontDec;// 字体减小
+- (void)WLMenu_setting_fontAdd;// 字体增加
+- (void)WLMenu_setting_fan;// 繁还是简
+- (void)WLMenu_setting_T;// 字体
+- (void)WLMenu_setting_spaceWithValue:(NSInteger)i;// space
+- (void)WLMenu_setting_eye;// 护眼模式
+- (void)WLMenu_setting_colorWithValue:(NSInteger)i;// 颜色设置
+- (void)WLMenu_setting_more;// 更多设置
+
+
+//
+
+
+
+@end
 
 @interface WLMenu : UIView
+
+@property (nonatomic, weak)id delegate;
 
 @property (nonatomic, strong)IBOutlet UIView *header;
 @property (nonatomic, strong)IBOutlet UIView *footer;
 @property (nonatomic, strong)IBOutlet UIButton *floatBtn;
 
+///
+
+@property (nonatomic, strong)IBOutlet UIView *darkBackground;
+
+@property (nonatomic, strong)IBOutlet UISlider *slider;
+
+@property (nonatomic, strong)IBOutlet UIButton *fontDec;
+@property (nonatomic, strong)IBOutlet UIButton *fontAdd;
+@property (nonatomic, strong)IBOutlet UIButton *fontFan;
+@property (nonatomic, strong)IBOutlet UIButton *fontT;
+
+@property (nonatomic, strong)IBOutlet UIButton *fontSpaceSmall;
+@property (nonatomic, strong)IBOutlet UIButton *fontSpaceNormal;
+@property (nonatomic, strong)IBOutlet UIButton *fontSpaceBig;
+@property (nonatomic, strong)IBOutlet UIButton *fontEye;
+
+@property (nonatomic, strong)IBOutlet UIButton *colorWhite;
+@property (nonatomic, strong)IBOutlet UIButton *colorGray;
+@property (nonatomic, strong)IBOutlet UIButton *colorGreen;
+@property (nonatomic, strong)IBOutlet UIButton *colorYellow;
+@property (nonatomic, strong)IBOutlet UIButton *colorPink;
+
+@property (nonatomic, strong)IBOutlet UIImageView *colorWhiteCheck;
+@property (nonatomic, strong)IBOutlet UIImageView *colorGrayCheck;
+@property (nonatomic, strong)IBOutlet UIImageView *colorGreenCheck;
+@property (nonatomic, strong)IBOutlet UIImageView *colorYellowCheck;
+@property (nonatomic, strong)IBOutlet UIImageView *colorPinkCheck;
+
+@property (nonatomic, strong)IBOutlet UIButton *more;
+
+
+
 - (IBAction)clickBackground:(id)sender;
+
+// 设置配置
+- (void)setData:(NSDictionary *)dic;
 
 @end
--- WLMenu.m
+++ WLMenu.m
@@ -7,8 +7,109 @@
 //
 
 #import "WLMenu.h"
+#import "UIImage+RTTint.h"
 
 @implementation WLMenu
+
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    UIView *bg = self.fontFan.superview;
+    bg.frame = self.darkBackground.frame;
+
+    for (UIView *view in [bg subviews]) {
+        [self reSetFrame:view];
+    }
+    
+}
+
+- (void)reSetFrame:(UIView *)view {
+    CGFloat delta = [UIScreen mainScreen].bounds.size.width/375.0;
+    
+    CGRect frame = view.frame;
+    CGRect newFrame = CGRectMake(frame.origin.x*delta, frame.origin.y*delta, frame.size.width * delta, frame.size.height * delta);
+    view.frame = newFrame;
+    
+    if (view.tag >= 200) {
+        view.layer.cornerRadius = 22*delta;
+    }
+}
+
+- (IBAction)setting:(id)sender {
+    self.darkBackground.hidden = !self.darkBackground.hidden;
+    [[self viewWithTag:100] setHidden:![[self viewWithTag:100] isHidden]];
+}
+- (IBAction)setting2:(id)sender {
+    [self setting:sender];
+}
+
+// 设置配置
+- (void)setData:(NSDictionary *)dic1 {
+    
+    NSDictionary *dic = @{@"brightness":@(0.67),
+            @"fan":@(1),
+            @"lineSpacing":@(2),
+            @"eye":@(1),
+            @"color":@(2)};
+    
+    
+    CGFloat brightness = [dic[@"brightness"] floatValue];
+    NSInteger fan = [dic[@"fan"] integerValue];
+    NSInteger lineSpacing = [dic[@"lineSpacing"] integerValue];
+    NSInteger eye = [dic[@"eye"] integerValue];
+    NSInteger color = [dic[@"color"] integerValue];
+    
+    
+    self.slider.value = brightness;
+    if (fan == 0) {
+        [self.fontFan setImage:[UIImage imageNamed:@"setting_font_fan"] forState:UIControlStateNormal];
+    } else {
+        [self.fontFan setImage:[UIImage imageNamed:@"setting_font_jian"] forState:UIControlStateNormal];
+    }
+    
+    [self.fontSpaceSmall setImage:[UIImage imageNamed:@"setting_space_small"] forState:UIControlStateNormal];
+    [self.fontSpaceNormal setImage:[UIImage imageNamed:@"setting_space_normal"] forState:UIControlStateNormal];
+    [self.fontSpaceBig setImage:[UIImage imageNamed:@"setting_space_big"] forState:UIControlStateNormal];
+    
+    if (lineSpacing == 0) {
+        [self.fontSpaceSmall setImage:[UIImage imageNamed:@"setting_space_small_selected"] forState:UIControlStateNormal];
+    } else if (lineSpacing == 1) {
+        [self.fontSpaceNormal setImage:[UIImage imageNamed:@"setting_space_normal_selected"] forState:UIControlStateNormal];
+    } else if (lineSpacing == 2) {
+        [self.fontSpaceBig setImage:[UIImage imageNamed:@"setting_space_big_selected"] forState:UIControlStateNormal];
+    }
+    
+    if (eye == 0) {
+        UIImage *image = [UIImage imageNamed:@"setting_eye"];
+        UIImage *tinted = image;
+        [self.fontEye setImage:tinted forState:UIControlStateNormal];
+    } else {
+        UIImage *image = [UIImage imageNamed:@"setting_eye"];
+        UIImage *tinted = [image rt_tintedImageWithColor:Red];
+        [self.fontEye setImage:tinted forState:UIControlStateNormal];
+    }
+    
+    
+    self.colorWhiteCheck.hidden = YES;
+    self.colorGrayCheck.hidden = YES;
+    self.colorGreenCheck.hidden = YES;
+    self.colorYellowCheck.hidden = YES;
+    self.colorPinkCheck.hidden = YES;
+    
+    if (color == 0) {
+        self.colorWhiteCheck.hidden = NO;
+    } else if (color == 1) {
+        self.colorGrayCheck.hidden = NO;
+    } else if (color == 2) {
+        self.colorGreenCheck.hidden = NO;
+    } else if (color == 3) {
+        self.colorYellowCheck.hidden = NO;
+    } else if (color == 4) {
+        self.colorPinkCheck.hidden = NO;
+    }
+    
+}
 
 - (IBAction)clickBackground:(id)sender {
     NSLog(@"menu!");
@@ -20,7 +121,9 @@
         [UIView animateWithDuration:0.3 animations:^{
             self.header.center = CGPointMake(self.header.center.x, self.header.center.y-64);
             self.footer.center = CGPointMake(self.footer.center.x, self.footer.center.y+55);
-            self.floatBtn.center = CGPointMake(self.floatBtn.center.x, self.floatBtn.center.y+164);
+            self.floatBtn.center = CGPointMake(self.floatBtn.center.x, self.floatBtn.center.y+130);
+            [[self viewWithTag:100] setHidden:YES];
+            self.darkBackground.hidden = YES;
             [[UIApplication sharedApplication] setStatusBarHidden:YES animated:YES];
             
         } completion:^(BOOL finished) {
@@ -32,7 +135,9 @@
         [UIView animateWithDuration:0.3 animations:^{
             self.header.center = CGPointMake(self.header.center.x, self.header.center.y+64);
             self.footer.center = CGPointMake(self.footer.center.x, self.footer.center.y-55);
-            self.floatBtn.center = CGPointMake(self.floatBtn.center.x, self.floatBtn.center.y-164);
+            self.floatBtn.center = CGPointMake(self.floatBtn.center.x, self.floatBtn.center.y-130);
+            [[self viewWithTag:100] setHidden:YES];
+            self.darkBackground.hidden = YES;
             [[UIApplication sharedApplication] setStatusBarHidden:NO animated:YES];
             [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
             
@@ -43,5 +148,158 @@
         
     }
 }
+
+
+- (IBAction)clickAlertBackground:(id)sender {
+    UIView *bg = (UIView *)sender;
+    bg.hidden = YES;
+    UIView *v102 = [self viewWithTag:102]; // 书籍报错
+    UIView *v103 = [self viewWithTag:103];
+    v102.hidden = YES;
+    v103.hidden = YES;
+    
+}
+
+- (IBAction)feedback:(id)sender {
+    UIView *bg = [self viewWithTag:101];
+    bg.hidden = NO;
+    UIView *v102 = [self viewWithTag:102]; // 书籍报错
+    UIView *v103 = [self viewWithTag:103];
+    v102.hidden = NO;
+    
+}
+- (IBAction)feedback2:(id)sender {
+    [self feedback:sender];
+}
+
+
+- (IBAction)bookerror:(id)sender {
+    UIView *v103 = [self viewWithTag:103];
+    v103.hidden = NO;
+}
+
+
+- (IBAction)callback_advise:(id)sender {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_advise)]) {
+        [self.delegate WLMenu_advise];
+    }
+}
+
+- (IBAction)callback_bookerrorWithIndex:(id)sender {
+    NSInteger tag = [sender tag];
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_bookerrorWithIndex:)]) {
+        [self.delegate WLMenu_bookerrorWithIndex:tag];
+    }
+}
+
+
+
+- (IBAction)download:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:nil message:@"缓存多少章节？" preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"缓存50章" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_download50)]) {
+            [self.delegate WLMenu_download50];
+        }
+    }];
+    
+    UIAlertAction *action2 = [UIAlertAction actionWithTitle:@"缓存100章" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_download100)]) {
+            [self.delegate WLMenu_download100];
+        }
+    }];
+    
+    UIAlertAction *action3 = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    
+    [alert addAction:action];
+    [alert addAction:action2];
+    [alert addAction:action3];
+    
+    [[[[UIApplication sharedApplication] delegate] window].rootViewController presentViewController:alert animated:YES completion:nil];
+    
+                                
+}
+
+- (IBAction)download2:(id)sender {
+    [self download:sender];
+}
+
+
+- (IBAction)callback_sliderChanged:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    CGFloat value = slider.value;
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_lightWithValue:)]) {
+        [self.delegate WLMenu_setting_lightWithValue:value];
+    }
+}
+
+- (IBAction)callback_fontDec {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_fontDec)]) {
+        [self.delegate WLMenu_setting_fontDec];
+    }
+}
+
+- (IBAction)callback_fontAdd{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_fontAdd)]) {
+        [self.delegate WLMenu_setting_fontAdd];
+    }
+}
+
+- (IBAction)callback_jian {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_fan)]) {
+        [self.delegate WLMenu_setting_fan];
+    }
+}
+
+- (IBAction)callback_T {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_T)]) {
+        [self.delegate WLMenu_setting_T];
+    }
+}
+
+- (IBAction)callback_space_small {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_spaceWithValue:)]) {
+        [self.delegate WLMenu_setting_spaceWithValue:0];
+    }
+}
+
+- (IBAction)callback_space_normal {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_spaceWithValue:)]) {
+        [self.delegate WLMenu_setting_spaceWithValue:1];
+    }
+}
+
+- (IBAction)callback_space_big {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_spaceWithValue:)]) {
+        [self.delegate WLMenu_setting_spaceWithValue:2];
+    }
+}
+
+- (IBAction)callback_eye {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_eye)]) {
+        [self.delegate WLMenu_setting_eye];
+    }
+}
+
+- (IBAction)callback_color:(id)sender {
+    UIView *view = (UIView *)sender;
+    NSInteger tag = view.tag;
+    
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_colorWithValue:)]) {
+        [self.delegate WLMenu_setting_colorWithValue:tag-200];
+    }
+}
+
+- (IBAction)callback_more {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(WLMenu_setting_more)]) {
+        [self.delegate WLMenu_setting_more];
+    }
+}
+
+
+
+
+
 
 @end
